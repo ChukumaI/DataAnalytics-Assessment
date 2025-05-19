9cd0bbc3ac54a2cf6bd9b7537122d859fd2cd13c
ChukumaI <chuka.isaac@gmail.com>
2025-05-19 20:49:10 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -1,1 +1,11 @@
+Q3: List savings and investment accounts with no inflows in over a year
 
+SELECT 
+  s.id AS plan_id,
+  s.owner_id,
+  'Savings' AS type,
+  MAX(s.transaction_date) AS last_transaction_date,
+  DATEDIFF(CURDATE(), MAX(s.transaction_date)) AS inactivity_days
+FROM savings_savingsaccount s
+GROUP BY s.id, s.owner_id
+HAVING inactivit
